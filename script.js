a984bb2ad657e9bf505d79f96306482090a04457
tqjonathan <ja.toctaguano@alumnos.urjc.es>
2019-02-14 12:19:38 +0100

    commit

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function main() {
   console.log("Comienza JS");
 
-  var cero = document.getElementById('0')
+  //Nueva variable para coger el nº 0 [id='Cero']
+  var Cero = document.getElementById('Nº0')
 
-  cero.onclick = () => {
-    console.log("Cero");
+/* Array de prueba */
+  var Elementos = [];
+  Elementos.push(Nº0);
+  //Al hacer click en 0, muestra 0 en consola
+  Cero.onclick = () => {
+    console.log("0");
 
-    var display = document.getElementById('Display');
-    Display.innerHTML = '0'
+  //  var display = document.getElementById('Display');
+    Display.innerHTML = Elementos;
   }
 
 }
