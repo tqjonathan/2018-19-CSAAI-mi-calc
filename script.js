61babd2b4cc0098fa124f4bc23875aca0d6112fd
tqjonathan <ja.toctaguano@alumnos.urjc.es>
2019-02-14 11:17:37 +0100

    commit2

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function main() {
   console.log("Comienza JS");
 
+  var cero = document.getElementById('0')
+
+  cero.onclick = () => {
+    console.log("Cero");
+
+    var display = document.getElementById('Display');
+    Display.innerHTML = '0'
+  }
+
 }
