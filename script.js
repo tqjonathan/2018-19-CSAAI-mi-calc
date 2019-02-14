a78cb837be1617478b0bc128a05c7708a9e6489a
tqjonathan <ja.toctaguano@alumnos.urjc.es>
2019-02-14 19:56:05 +0100

    commit

--- script.js
+++ script.js
@@ -2,17 +2,26 @@ function main() {
   console.log("Comienza JS");
 
   //Nueva variable para coger el nº 0 [id='Cero']
-  var Cero = document.getElementById('Nº0')
+  var Cero = document.getElementById("Nº0");
+  var Uno = document.getElementById("Nº1");
+  var Dos = document.getElementById("Nº2");
+  var Tres = document.getElementById("Nº3");
+  var Cuatro = document.getElementById("Nº4");
+  var Cinco = document.getElementById("Nº5");
+  var Seis = document.getElementById("Nº6");
+  var Siete = document.getElementById("Nº7");
+  var Ocho = document.getElementById("Nº8");
+  var Nueve = document.getElementById("Nº9");
 
-/* Array de prueba */
-  var Elementos = [];
-  Elementos.push(Nº0);
-  //Al hacer click en 0, muestra 0 en consola
+  var Display = document.getElementById('Display');
+
   Cero.onclick = () => {
-    console.log("0");
-
-  //  var display = document.getElementById('Display');
-    Display.innerHTML = Elementos;
+  Display.innerHTML = Display.innerHTML + 0;
   }
+
+  Uno.onclick = () => {
+  Display.innerHTML = Display.innerHTML + 1;
+  }
+
 
 }
